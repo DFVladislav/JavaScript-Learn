7415f053675a562050ab0beede5e4a9ea62af9f2
DFedosov <fedya12346598@mail.ru>
2026-03-05 15:37:50 +0300

    newtask.js complete

---
+++ newtasks.js
@@ -0,0 +1,51 @@
+//  Заддача 1
+
+// const myButton = document.querySelector("button")
+//   const textP = document.querySelector('p')
+//   myButton.onclick = () => textP.textContent = "New text"
+
+
+// Задача 2
+// const textP = document.querySelector("p")
+
+// textP.addEventListener('mouseenter', () => textP.classList.add("activeMouse"))
+//   textP.addEventListener('mouseleave', () => textP.classList.remove("activeMouse"))
+
+// Задача 3\
+
+// const textDiv = document.querySelector("div")
+// const myButton = document.querySelector("button")
+
+// myButton.addEventListener('click', () => {
+//     textDiv.style.display = "none"
+// })
+
+
+// Задача 4
+
+// const myFirstButton = document.querySelector(".firstButton")
+// const mySecondButton = document.querySelector(".secondButton")
+// const myThirdButton = document.querySelector(".thirdButton")
+
+// const myFirstDiv = document.querySelector(".firstDiv")
+// const mySecondDiv = document.querySelector(".secondDiv")
+// const myThirdDiv = document.querySelector(".thirdDiv")
+
+// myFirstButton.addEventListener("click", () => myFirstDiv.classList.toggle("oneDiv"))
+// mySecondButton.addEventListener("click", () => mySecondDiv.classList.toggle("twoDiv"))
+// myThirdButton.addEventListener("click", () => myThirdDiv.classList.toggle("threeDiv"))
+
+
+// Задача 5 (НЕДОДЕЛАЛ)
+
+// const Input = document.querySelector(".itemInput")
+// const inputButton = document.querySelector(".addButton")
+// const list = document.querySelector(".myList")
+
+
+
+// const elementLi = document.createElement("li")
+// const buttonDelete = document.createElement("button")
+
+// elementLi.textContent = Input.value
+// buttonDelete.textContent="Удалить"
--- script.js
+++ script.js
@@ -141,12 +141,12 @@
 // sayHi(user); 
 
 
-const users = [
-  { name: "Аня", age: 17 },
-  { name: "Игорь", age: 25 },
-  { name: "Олег", age: 14 },
-  { name: "Маша", age: 19 }
-];
+// const users = [
+//   { name: "Аня", age: 17 },
+//   { name: "Игорь", age: 25 },
+//   { name: "Олег", age: 14 },
+//   { name: "Маша", age: 19 }
+// ];
 
 // users.forEach(function(user) {
 //     if (user.age > 18) {
@@ -154,6 +154,72 @@ const users = [
 //     }
 // });
 
-users.forEach(function(user){
-    
-})
+
+// const total = {
+//   price: 1000,
+//   count: 2,
+//   discount: 10
+// }
+// function calculateTotal(func) {
+//  let sum = (func.price * func.count) 
+//  let discountSum = sum * (func.discount / 100)
+//   return sum - discountSum
+// }
+// let result = calculateTotal(total)
+// console.log(result)
+
+
+// const arrayFirst = [-3, 0, 2, 10, "hello"]
+// const arraySecond = [5, 1, "world"]
+// const arrayThird = [12, 15, 22]
+// // слияние и срез массивов
+// // concat()
+// //slice()
+// const newArray = arrayFirst.slice(2)
+// console.log(newArray)
+
+// const firstDiv = document.querySelector("div")
+// // firstDiv.textContent = "HELLOO WWOORL"
+// // firstDiv.style.background = "blue"
+// // firstDiv.innerHTML = "<h1> Hello world</h1>"
+// const firstP = document.createElement("p")
+// firstDiv.prepend(firstP)
+// firstP.textContent = "First"
+
+// const secondP = document.createElement("p")
+// firstDiv.append(secondP)
+// secondP.textContent = "First"
+
+// const thirdP = document.createElement("p")
+// firstDiv.append(thirdP)
+// thirdP.textContent = "First"
+
+
+
+
+// const firstDiv = document.querySelector("div")
+// for (let i = 0; i < 5; i++) {
+//   const p = document.createElement("p")
+//   firstDiv.append(p)
+//   // p.textContent = "P"
+//   const allP = document.querySelectorAll("p")
+//   allP.forEach(element => {
+//     element.textContent = "Hello"
+//     console.log(allP)
+//   })
+// }
+
+// const firstDiv = document.querySelector("div")
+// // firstDiv.classList.add("div")
+// // firstDiv.classList.remove("div")
+// firstDiv.classList.toggle("firstDiv")
+
+
+// const clicker = () => {
+//   const div = document.querySelector("div")
+//   div.classList.toggle("black")
+// }
+
+// document.querySelector("button").addEventListener("mouseover", clicker)
+
+
